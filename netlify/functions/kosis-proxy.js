4de01105e8f5ccf93a39603c4ff51d3c9833dfc6
osh9607-collab <osh9607@gmail.com>
2026-06-09 16:06:51 +0900

    Update kosis-proxy.js

--- netlify/functions/kosis-proxy.js
+++ netlify/functions/kosis-proxy.js
@@ -1,5 +1,5 @@
 const https = require('https');
-
+ 
 exports.handler = async (event) => {
   const params = event.queryStringParameters || {};
   const orgId = params.orgId || '116';
@@ -8,10 +8,23 @@ exports.handler = async (event) => {
   const startPrdDe = params.startPrdDe || '200701';
   const endPrdDe = params.endPrdDe || '202612';
   const API_KEY = 'MTI0YzJmMjQzNTg1OGQwYzczNTEzYmY2NDk3MGQxY2Q=';
-
-  const url = `https://kosis.kr/openapi/Param/statisticsParameterData.do?method=getList&apiKey=${API_KEY}&itmId=13103871089T1+&objL1=ALL&objL2=ALL&objL3=13102871089C.0002&objL4=&objL5=&objL6=&objL7=&objL8=&format=json&jsonVD=Y&prdSe=${prdSe}&startPrdDe=${startPrdDe}&endPrdDe=${endPrdDe}&orgId=${orgId}&tblId=${tblId}`;
-
-  return new Promise((resolve, reject) => {
+ 
+  // 통계표별 파라미터 분기
+  let itmId, objParams;
+ 
+  if (tblId === 'DT_MLTM_1948') {
+    // 주택유형별 인허가 - C1=시도, C2=대분류, C3=중분류, C4=소분류
+    itmId = '13103871090T1+';
+    objParams = 'objL1=ALL&objL2=ALL&objL3=ALL&objL4=ALL&objL5=&objL6=&objL7=&objL8=';
+  } else {
+    // DT_MLTM_1946 부문별 인허가 - 서울(C3) 고정
+    itmId = '13103871089T1+';
+    objParams = 'objL1=ALL&objL2=ALL&objL3=13102871089C.0002&objL4=&objL5=&objL6=&objL7=&objL8=';
+  }
+ 
+  const url = `https://kosis.kr/openapi/Param/statisticsParameterData.do?method=getList&apiKey=${API_KEY}&itmId=${itmId}&${objParams}&format=json&jsonVD=Y&prdSe=${prdSe}&startPrdDe=${startPrdDe}&endPrdDe=${endPrdDe}&orgId=${orgId}&tblId=${tblId}`;
+ 
+  return new Promise((resolve) => {
     https.get(url, (res) => {
       let data = '';
       res.on('data', (chunk) => { data += chunk; });
@@ -20,10 +33,7 @@ exports.handler = async (event) => {
           const parsed = JSON.parse(data);
           resolve({
             statusCode: 200,
-            headers: {
-              'Access-Control-Allow-Origin': '*',
-              'Content-Type': 'application/json'
-            },
+            headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
             body: JSON.stringify(parsed)
           });
         } catch (e) {
