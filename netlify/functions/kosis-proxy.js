fb15cab63cba0110ac594fa15a38fd0a1f8a149c
osh9607-collab <osh9607@gmail.com>
2026-06-09 16:15:27 +0900

    Update kosis-proxy.js

--- netlify/functions/kosis-proxy.js
+++ netlify/functions/kosis-proxy.js
@@ -1,5 +1,5 @@
 const https = require('https');
- 
+
 exports.handler = async (event) => {
   const params = event.queryStringParameters || {};
   const orgId = params.orgId || '116';
@@ -8,22 +8,21 @@ exports.handler = async (event) => {
   const startPrdDe = params.startPrdDe || '200701';
   const endPrdDe = params.endPrdDe || '202612';
   const API_KEY = 'MTI0YzJmMjQzNTg1OGQwYzczNTEzYmY2NDk3MGQxY2Q=';
- 
-  // 통계표별 파라미터 분기
+
   let itmId, objParams;
- 
+
   if (tblId === 'DT_MLTM_1948') {
-    // 주택유형별 인허가 - C1=시도, C2=대분류, C3=중분류, C4=소분류
+    // 주택유형별 - 서울(C1=13102871090A.0003) 고정
     itmId = '13103871090T1+';
-    objParams = 'objL1=ALL&objL2=ALL&objL3=ALL&objL4=ALL&objL5=&objL6=&objL7=&objL8=';
+    objParams = 'objL1=13102871090A.0003&objL2=ALL&objL3=ALL&objL4=ALL&objL5=&objL6=&objL7=&objL8=';
   } else {
-    // DT_MLTM_1946 부문별 인허가 - 서울(C3) 고정
+    // 부문별 - 서울(C3=13102871089C.0002) 고정
     itmId = '13103871089T1+';
     objParams = 'objL1=ALL&objL2=ALL&objL3=13102871089C.0002&objL4=&objL5=&objL6=&objL7=&objL8=';
   }
- 
+
   const url = `https://kosis.kr/openapi/Param/statisticsParameterData.do?method=getList&apiKey=${API_KEY}&itmId=${itmId}&${objParams}&format=json&jsonVD=Y&prdSe=${prdSe}&startPrdDe=${startPrdDe}&endPrdDe=${endPrdDe}&orgId=${orgId}&tblId=${tblId}`;
- 
+
   return new Promise((resolve) => {
     https.get(url, (res) => {
       let data = '';
